e27ede86ae60f0fa5d5911ca5f06b8ad5aa0dd11
Suman Rai <officeproj111@gmail.com>
2024-01-25 01:47:16 +0545

    Initial Commit

---
+++ BLL/AssemblyReferences.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public static class AssemblyReferences
+    {
+        public static IServiceCollection AddBLL(this IServiceCollection services)
+        { 
+            return services;
+        }
+    }
+}
