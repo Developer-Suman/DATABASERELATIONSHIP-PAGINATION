0dbfbd8613543ebf2b4c605f72a556f2824a3ce6
Suman Rai <officeproj111@gmail.com>
2024-01-26 17:11:21 +0545

    Configured Identity

---
+++ DAL/AssemblyReferences.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper.Configuration;
+using DAL.DbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class AssemblyReferences
+    {
+        public static IServiceCollection AddDAL(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("DAL"));
+
+            });
+            return services;
+        }
+    }
+}
---
+++ DAL/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DbContext
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
